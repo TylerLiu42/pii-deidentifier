472743d7434be7251ab96f0b97b4dc9a34f23fb3
TylerLiu42 <tgqliu@uwaterloo.ca>
2021-05-06 10:53:53 -0400

    Fix http codes

--- src/main.py
+++ src/main.py
@@ -10,6 +10,8 @@ CLOUD_STORAGE_BUCKET = os.environ['CLOUD_STORAGE_BUCKET']
 @app.route('/upload', methods=['POST'])
 def upload():
     uploaded_file = request.files['file']
+    if uploaded_file is None:
+        return jsonify(message="File not found"), 400
     # Create a Cloud Storage client.
     gcs = storage.Client()
 
@@ -23,7 +25,7 @@ def upload():
         uploaded_file.read(),
         content_type=uploaded_file.content_type
     )
-    return 200, jsonify(message="Success")
+    return jsonify(message="Success"), 200
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
